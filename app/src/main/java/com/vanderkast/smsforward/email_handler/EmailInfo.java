552840862f3bb293bfe49261e8705c05f719aff3
Vanderkast <vanderkast@yandex.ru>
2020-08-17 10:29:24 +0300

    EmailInfo ignore update

--- app/src/main/java/com/vanderkast/smsforward/email_handler/EmailInfo.java
+++
@@ -1,6 +0,0 @@
-package com.vanderkast.smsforward.email_handler;
-
-public class EmailInfo {
-    public static final String address = "ftsolution.sms@gmail.com";
-    public static final String password  = "qwertyZ9228";
-}
